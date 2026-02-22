ed76375dcdb4dce6639f1f88cc135c4f609b8389
mfcworks <mfcworks@outlook.jp>
2026-02-22 22:42:36 +0900

    Restore

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -3,13 +3,13 @@ const { site } = useAppConfig()
 
 const { data: page } = await useAsyncData('about', () => queryCollection('content').path('/about/').first())
 
-// useHead({
-//   style: [`
-//     .feature {
-//       background-image: url(${page.value.feature});
-//     }`
-//   ]
-// })
+useHead({
+  style: [`
+    .feature {
+      background-image: url(${page.value.feature});
+    }`
+  ]
+})
 </script>
 
 <template>
@@ -20,13 +20,13 @@ const { data: page } = await useAsyncData('about', () => queryCollection('conten
         <div class="wrapper animated fadeIn">
             <div class="content">
                 <div :class="`post-title ${page.feature ? 'feature' : ''}`">
-                    <!-- <h1>{{ page.title }}</h1> -->
+                    <h1>{{ page.title }}</h1>
                     <a class="btn zoombtn" href="/">
                         <i class="fa fa-chevron-left"></i>
                     </a>
                 </div>
-                <!-- <ContentRenderer v-if="page" :value="page" /> -->
-                <!-- <div v-else>Page not found</div> -->
+                <ContentRenderer v-if="page" :value="page" />
+                <div v-else>Page not found</div>
             </div>
         </div>
         <!-- {% if page.comments and site.disqus_shortname %}<section id="disqus_thread" class="animated fadeInUp"></section>/#disqus_thread{% endif %} -->
